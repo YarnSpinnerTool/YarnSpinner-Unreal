915efecbc2bb9ab0b5f860ea657db376cd64f924
Jon Manning <jon@secretlab.com.au>
2022-05-06 12:12:52 +1000

    Create YarnSpinner module, add as dep to primary harness module

--- Source/YarnSpinnerHarness/YarnSpinnerHarness.Build.cs
+++ Source/YarnSpinnerHarness/YarnSpinnerHarness.Build.cs
@@ -8,7 +8,7 @@ public class YarnSpinnerHarness : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "StaticParty" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "StaticParty", "YarnSpinner" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
