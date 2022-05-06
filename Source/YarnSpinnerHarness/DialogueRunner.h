4b26348458996be7af1178b534bc6be77b3076b2
Jon Manning <jon@secretlab.com.au>
2022-05-06 12:07:56 +1000

    rename primary game module to 'YarnSpinnerHarness'

---
+++ Source/YarnSpinnerHarness/DialogueRunner.h
@@ -0,0 +1,52 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "DialogueRunner.generated.h"
+
+UCLASS()
+class YARNSPINNERHARNESS_API ADialogueRunner : public AActor
+{
+    GENERATED_BODY()
+    
+public:
+    // Sets default values for this actor's properties
+    ADialogueRunner();
+
+protected:
+    // Called when the game starts or when spawned
+    virtual void BeginPlay() override;
+
+public:
+    // Called every frame
+    virtual void Tick(float DeltaTime) override;
+    
+    UFUNCTION(BlueprintNativeEvent, Category="Dialogue Runner")
+    void OnDialogueStarted();
+    
+    UFUNCTION(BlueprintNativeEvent, Category="Dialogue Runner")
+    void OnDialogueEnded();
+    
+    UFUNCTION(BlueprintNativeEvent, Category="Dialogue Runner")
+    void OnRunLine(class ULine* line);
+    
+    UFUNCTION(BlueprintCallable)
+    void StartDialogue(FName nodeName);
+    
+    UFUNCTION(BlueprintCallable)
+    void ContinueDialogue();
+    
+    UFUNCTION(BlueprintCallable)
+    void SelectOption(int optionID);
+    
+private:
+    UPROPERTY(VisibleInstanceOnly)
+    int32 currentContentIndex = 0;
+    
+    class ULine* GetFakeLine(int index);
+    
+    class ULine* MakeFakeLine(FName lineID);
+
+};
---
+++ Source/YarnSpinnerHarness/Line.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "Line.generated.h"
+
+/**
+ *
+ */
+UCLASS(BlueprintType)
+class YARNSPINNERHARNESS_API ULine : public UObject
+{
+    GENERATED_BODY()
+    
+public:
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+    FName LineID;
+    
+};
