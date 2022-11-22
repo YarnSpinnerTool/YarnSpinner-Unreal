4fac41edd4965fd5a883a227fe55256019e666a8
Jon Manning <jon@secretlab.com.au>
2022-11-22 15:29:56 +1100

    Make root of project the Yarn Spinner plugin

--- Source/YarnSpinnerHarness/YarnSpinnerHarness.cpp
+++
@@ -1,7 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "YarnSpinnerHarness.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, YarnSpinnerHarness, "YarnSpinnerHarness" );
-
